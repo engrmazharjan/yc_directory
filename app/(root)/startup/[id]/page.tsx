f2bd2c0d9d1977eea2135ad7b7f9635c105b66dc
engrmazharjan <mazharj4565@gmail.com>
2024-10-31 01:44:11 +0500

    Editors picks startups

--- app/(root)/startup/[id]/page.tsx
+++ app/(root)/startup/[id]/page.tsx
@@ -20,14 +20,12 @@ const md = markdownit();
 const Page = async ({ params }: { params: Promise<{ id: string }> }) => {
   const id = (await params).id;
 
-  // const [post, { select: editorPosts }] = await Promise.all([
-  //   client.fetch(STARTUP_BY_ID_QUERY, { id }),
-  //   client.fetch(PLAYLIST_BY_SLUG_QUERY, {
-  //     slug: "editor-picks-new",
-  //   }),
-  // ]);
-
-  const post = await client.fetch(STARTUP_BY_ID_QUERY, { id });
+  const [post, { select: editorPosts }] = await Promise.all([
+    client.fetch(STARTUP_BY_ID_QUERY, { id }),
+    client.fetch(PLAYLIST_BY_SLUG_QUERY, {
+      slug: "editor-picks-new",
+    }),
+  ]);
 
   if (!post) return notFound();
 
@@ -87,7 +85,7 @@ const Page = async ({ params }: { params: Promise<{ id: string }> }) => {
 
         <hr className="divider" />
 
-        {/* {editorPosts?.length > 0 && (
+        {editorPosts?.length > 0 && (
           <div className="max-w-4xl mx-auto">
             <p className="text-30-semibold">Editor Picks</p>
 
@@ -97,7 +95,7 @@ const Page = async ({ params }: { params: Promise<{ id: string }> }) => {
               ))}
             </ul>
           </div>
-        )} */}
+        )}
 
         <Suspense fallback={<Skeleton className="view_skeleton" />}>
           <View id={id} />
