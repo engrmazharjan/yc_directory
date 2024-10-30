474142cf99c540e0cbc3ce8a0cdf82a54b764135
engrmazharjan <mazharj4565@gmail.com>
2024-10-30 22:46:15 +0500

    Author authentication flow

--- sanity/lib/queries.ts
+++ sanity/lib/queries.ts
@@ -62,3 +62,14 @@ export const PLAYLIST_BY_SLUG_QUERY =
   }
 }`);
 
+export const AUTHOR_BY_GITHUB_ID_QUERY = defineQuery(`
+  *[_type == "author" && id == $id][0]{
+      _id,
+      id,
+      name,
+      username,
+      email,
+      image,
+      bio
+  }
+  `);
